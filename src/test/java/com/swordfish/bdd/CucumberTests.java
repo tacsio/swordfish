7a1d0f18e559a04b6493de064896ec26b7e1dfe6
Tarcisio Coutinho <tcs5cin@gmail.com>
2017-09-12 09:32:30 -0300

    add jvm cucumber option (problema com scenario outlines)

--- src/test/java/com/swordfish/bdd/CucumberTests.java
+++ src/test/java/com/swordfish/bdd/CucumberTests.java
@@ -6,7 +6,7 @@ import cucumber.api.CucumberOptions;
 import cucumber.api.junit.Cucumber;
 
 @RunWith(Cucumber.class)
-@CucumberOptions(plugin = { "pretty" }, features = "src/test/resources", strict = true)
+@CucumberOptions(plugin = { "pretty" }, junit= {"--filename-compatible-names"}, features = "src/test/resources", strict = true)
 public class CucumberTests {
 
 }
